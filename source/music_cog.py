eb986e31cfedc2f80c7127ba19aee18206a9e9ea
Orlek222 <lukach.omar@gmail.com>
2024-03-31 17:48:54 +0200

    Music Cog done

--- source/music_cog.py
+++ source/music_cog.py
@@ -90,3 +90,31 @@ class music_cog(commands.Cog):
             self.isplaying = True
             self.ispaused = False
             self.vc.resume()
+
+    @commands.command(name="skip", aliases =["s"], help="Skips the current song")
+    async def skip(self, ctx, *args):
+        if self.vc != None and self.vc:
+            self.vc.stop()
+            await self.play_song(ctx)
+
+    @commands.command(name="queue", aliases=["q"], help="Shows the 5 first elements in queue")
+    async def queue(self, ctx, *args):
+        queue = ""
+        for i in range(0, len(self.music_queue)):
+            if i>5: break
+            queue += self.music_queue[i][0]['title'] + '\n'
+        if queue != "": await ctx.send(queue)
+        else: await ctx.send("No songs in queue")
+
+    @commands.command(name="clear", aliases=["c"], help="Clears the queue")
+    async def clear(self, ctx, *args):
+        if self.vc != None and self.isplaying:
+            self.vc.stop()
+        self.music_queue.clear()
+        await ctx.send("Queue cleared")
+
+    @commands.command(name="leave", aliases=["l"], help="Kick the bot from the voice channel")
+    async def leave(self, ctx, *args):
+        self.isplaying = False
+        self.ispaused = False
+        await self.vc.disconnect()
