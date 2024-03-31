5f362f9ac6d9168d06107a055d60528bd8fafdae
Orlek222 <lukach.omar@gmail.com>
2024-03-31 18:09:33 +0200

    Ready to use

--- source/main.py
+++ source/main.py
@@ -1,12 +1,23 @@
+##-----IMPORTS AND DEFINITIONS-----##
+
 import discord
 from discord.ext import commands
-import os
 import tokens as constants
+
+from helper_cog import HelperCog
+from music_cog import music_cog
 
 API_KEY = constants.TOKEN
 
-bot = commands.Bot(command_prefix="/")
-
 
 def main():
+    bot = commands.Bot(command_prefix="/")
+
+    bot.remove_command('help')
+    bot.add_cog(music_cog(bot))
+    bot.add_cog(HelperCog(bot))
+
     bot.run(API_KEY)
+
+
+main()
